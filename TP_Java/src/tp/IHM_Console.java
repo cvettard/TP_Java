4f0391110352fd15e40603071e258c2bab851938
cvettard <51156954+cvettard@users.noreply.github.com>
2020-12-08 11:31:24 +0100

    Finitions affichage + conditions de victoire

--- TP_Java/src/tp/IHM_Console.java
+++ TP_Java/src/tp/IHM_Console.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class IHM_Console implements Affichage {
 
 	private Sudoku jeuSudoku;
-	
+	private static Scanner scan = new Scanner(System.in);
 	public IHM_Console(Sudoku p_sudoku) {
 		this.jeuSudoku=p_sudoku;
 	}
@@ -18,18 +18,44 @@ public class IHM_Console implements Affichage {
 	@Override
 	public void SaisirChiffreEtCoordonneesCase() {
 		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		int chiffreSaisi;
-		int ligneSaisi;
-		int colonneSaisi;
+		int chiffreSaisi=-1;
+		int ligneSaisi=-1;
+		int colonneSaisi=-1;
+
+		//Saisi du chiffre à rentrer dans le sudoku (doit être compris entre 1 et 4)
+		while(chiffreSaisi<1 || chiffreSaisi>4) {
+			System.out.println("Saisir un chiffre compris entre 1 et 4 :");
+			try {
+				chiffreSaisi = scan.nextInt();
+			}
+			catch(Exception e) {
+				scan.nextLine();
+			}
+		}
 		
-		System.out.println("Veuillez saisir un chiffre compris entre 0 et 3 :");
-		chiffreSaisi = scan.nextInt();
-		System.out.println("Entrez le numero de ligne :");
-		ligneSaisi = scan.nextInt();
-		System.out.println("Entrez le numero de colonne :");
-		colonneSaisi = scan.nextInt();
+		//Saisi de l'abscisse (doit être compris entre 0 et 3)
+		while(ligneSaisi<0 || ligneSaisi>3) {
+			System.out.println("Entrez le numero de ligne (compris entre 0 et 3) :");
+			try {
+				ligneSaisi = scan.nextInt();
+			}
+			catch(Exception e) {
+				scan.nextLine();
+			}
+		}
 		
+		//Saisi de l'ordonnée (doit être compris entre 0 et 3)
+		while(colonneSaisi<0 || colonneSaisi>3) {
+			System.out.println("Entrez le numero de colonne (compris entre 0 et 3) :");
+			try {
+				colonneSaisi = scan.nextInt();
+			}
+			catch(Exception e) {
+				scan.nextLine();
+			}
+		}
+		
+		//Effectue les vérification pour savoir si l'on peut entrer ce chiffre ou non dans la grille
 		if(this.jeuSudoku.VerificationLigne(chiffreSaisi, ligneSaisi)
 			&& this.jeuSudoku.VerificationColonne(chiffreSaisi, colonneSaisi)
 			&& this.jeuSudoku.getGrille()[ligneSaisi][colonneSaisi].getChiffre()==0) {
@@ -40,23 +66,34 @@ public class IHM_Console implements Affichage {
 				System.out.println("Cette case ne peut pas être modifié !");
 			}
 			else {
-				System.out.println("Coordonées incorrect, doit être compris entre 0 et 3 !");
+				if(!this.jeuSudoku.VerificationLigne(chiffreSaisi, ligneSaisi)) {
+					System.out.println("Il y a déjà un "+chiffreSaisi+" dans cette ligne !");
+				}
+				else {
+					if(!this.jeuSudoku.VerificationColonne(chiffreSaisi, colonneSaisi)){
+						System.out.println("Il y a déjà un "+chiffreSaisi+" dans cette colonne !");
+					}
+				}
 			}
 			
 		}
-		scan.close();
-		this.jeuSudoku.VerificationGagne();
 	}
 
+	/*
+	 * Affiche la grille du Sudoku, les zeros sont des cases vides
+	 */
 	@Override
 	public void AffichageGrille() {
+		System.out.println("\n***********");
 		// TODO Auto-generated method stub
 		for(int iLigne=0; iLigne<this.jeuSudoku.getGrille().length;iLigne++) {
+			System.out.print("* ");
 			for(int iColonne=0; iColonne<this.jeuSudoku.getGrille().length;iColonne++) {
 				System.out.print(this.jeuSudoku.getGrille()[iLigne][iColonne].getChiffre()+" ");
 			}
-			System.out.println("");
+			System.out.print("*\n");
 		}
+		System.out.println("***********\n");
 	}
 
 }
